fbf29ff3f5705c5a5b494df26ab3109f73400c6e
Wevertonf45 <weverton07102005@gmail.com>
2025-11-07 22:14:15 -0300

    Update server.js

--- api-crud-usuários/src/server.js
+++ api-crud-usuários/src/server.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 //Configurações
 const app = express();
 app.use(cors({
-    origin: ['https://dashboard-green-one-73.vercel.app/cadastro', 'https://dashboard-green-one-73.vercel.app/'],
+    origin: ['https://dashboard-green-one-73.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
