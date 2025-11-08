e2d78ef1ba9b4aba6fff9546be750c0319ee9c4b
Wevertonf45 <weverton07102005@gmail.com>
2025-11-07 21:35:08 -0300

    vercel JSON

--- api-crud-usuários/src/server.js
+++ api-crud-usuários/src/server.js
@@ -7,9 +7,8 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 
-
-app.listen(3000, () => console.log('Server running on port 3000'));
-
 app.use(express.json());
 
 app.use('/', routes);
+
+export default app
